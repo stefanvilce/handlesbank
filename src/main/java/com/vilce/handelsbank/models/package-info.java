4a3e1be67b67e6d13993f5c22d1c21f03f129fb0
stefanvilce <stefan@irisoft.com.ro>
2024-06-05 07:49:34 +0200

    Delete package-info

--- src/main/java/com/vilce/handelsbank/models/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.vilce.handelsbank.models;
--- src/test/java/com/vilce/handelsbank/HandelsbankApplicationTests.java
+++ src/test/java/com/vilce/handelsbank/HandelsbankApplicationTests.java
@@ -1,7 +1,9 @@
 package com.vilce.handelsbank;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class HandelsbankApplicationTests {
@@ -9,5 +11,11 @@ class HandelsbankApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	/*
+	@Test
+	void testTest() {
+		System.out.println("PORT: " + WebEnvironment.RANDOM_PORT);
+		assertEquals(2, 2);
+	}*/
 
 }
